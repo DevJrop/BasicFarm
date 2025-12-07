ad2c9e994001923efd4b97f3a82029946d7c57fc
Juan Rodriguez <juanjrop002@gmail.com>
2025-12-07 23:30:42 +0100

    AssetPack and basic scripts created to develop fruitsystem

---
+++ Assets/Scripts/Fruit.cs
@@ -0,0 +1,11 @@
+using UnityEngine;
+
+[CreateAssetMenu(fileName = "Fruit", menuName = "Scriptable Objects/Fruit")]
+public class Fruit : ScriptableObject
+{
+    [SerializeField] FruitType type;
+    [SerializeField] float timeBetweenFruits;
+    [SerializeField] float maxCapacity;
+    [SerializeField] float cooldownToAvailable;
+    
+}
