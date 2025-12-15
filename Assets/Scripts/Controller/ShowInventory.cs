715d325ade74c36000d26e4ba3c007f839a842d4
Juan Rodriguez <juanjrop002@gmail.com>
2025-12-15 22:08:16 +0100

    Open and close system to the fruits inventory

---
+++ Assets/Scripts/Controller/ShowInventory.cs
@@ -0,0 +1,19 @@
+using UnityEngine;
+using UnityEngine.UIElements;
+
+public class ShowInventory : MonoBehaviour
+{
+    [SerializeField] private CanvasRenderer inventory;
+
+    public void OpenInventory()
+    {
+            inventory.gameObject.SetActive(true);
+            Debug.Log("Open Inventory");
+    }
+    
+    public void CloseInventory()
+    {
+        inventory.gameObject.SetActive(false);
+        Debug.Log("Close Inventory");
+    }
+}
