7901444d81f868839ce95f7429672a55693bc911
Juan Rodriguez <juanjrop002@gmail.com>
2025-12-15 16:53:00 +0100

    Cleaning Up folders. UI added, slots to put seeds and basket. also a mini inventory to count the fruits

---
+++ Assets/Scripts/Core/FruitRecollection.cs
@@ -0,0 +1,43 @@
+using System;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FruitRecollection : MonoBehaviour
+{
+    [SerializeField] private int fruitAmount;
+    
+    Dictionary<FruitType, int> fruitCounter = new Dictionary<FruitType, int>();
+
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+        if (!other.CompareTag("Fruit")) return;
+        
+        FruitHolder fruitHolder = other.GetComponent<FruitHolder>();
+        
+        if (fruitHolder == null) return;
+        
+            Fruit fruit = fruitHolder.FruitData;
+        
+            FruitType type = fruit.Type;
+            AddFruit(type);
+            Destroy(other.gameObject);
+    }
+
+    public void AddFruit(FruitType type )
+    {
+        fruitCounter.TryAdd(type, -1);
+
+        fruitCounter[type]++;
+        ShowCounter();
+    }
+
+    public void ShowCounter()
+    {
+        foreach (var fruit in fruitCounter)
+        {
+            Debug.Log(fruit.Key + ": " + fruit.Value);
+        }
+    }
+    
+    
+}
