ca44a16284b3082397ef8b845671582ab4ee9a1d
Juan Rodriguez <juanjrop002@gmail.com>
2025-12-29 17:42:47 +0100

    Prefab Tree was change and improved to be able to open a interface to look at how many fruits de tree has

--- Assets/Scripts/Controller/FruitGenerator.cs
+++ Assets/Scripts/Controller/FruitGenerator.cs
@@ -2,6 +2,12 @@ using UnityEngine;
 
 public class FruitGenerator : MonoBehaviour
 {
+    /* Al ser instanciado el arbol comienza a generar la fruta
+     * Generar 1 fruta por cantidad de maxima de fruta.
+     * al generar el maximo de frutas debe parar de generar
+     * al pasar la canasta por encima de los arboles debe salir la animacionde recollecion de frutas
+     *
+     */
 
 
     private void SpawnFruits()
